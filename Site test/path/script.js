468d3a355371468228de4772fba05680e623a921
hoperz <53231998+hoperz@users.noreply.github.com>
2019-10-18 09:01:23 +0300

    add file

---
+++ Site test/path/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.table(persone)
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+let answer = confirm("11");
+
+console.log(answer);
